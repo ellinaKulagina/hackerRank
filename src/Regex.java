e6aee09892ddf0e116c8c4d643c167e3a29c5b60
ELLINA\Ellina <ellina.kulagina@gmail.com>
2019-08-31 19:09:22 +0100

    regex puzzle

---
+++ src/Regex.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Regex {
+    public static void mainNotMain(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+
+    static class MyRegex {
+        final String pattern = "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+    }
+}
